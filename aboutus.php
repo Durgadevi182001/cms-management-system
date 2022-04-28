b9152fc6b2c820c8d24568928b3a48cea23bb58f
Durgadevi182001 <104543600+Durgadevi182001@users.noreply.github.com>
2022-04-28 08:25:32 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,22 @@
+<?php
+include("header.php");
+?>
+	
+	<!-- register -->
+	<div class="register-sec-w3l" id="register">
+		<div class="container">
+			<div class="col-md-6 book-appointment">
+				<h3>About Alvas College</h3>
+<p>24x7college Education Foundation is a multifaceted organization founded by Dr. M. Mohan Alva and it is considered as the crown of Moodbidri. It has 20 Educational institutions with more than 20,000 students. It is a trust registered under Trust Act with an objective of catering to the needs of cultural and educational development. It started its activity in the year 1995 with Nursing and Ayurveda streams of education and now it has primary to post-graduate courses in General Education, Paramedical Courses, Engineering and Management, Alternative Medicine encompassing students from all over India and abroad.</p>
+			</div>
+			<div class="col-md-6 image-left-w3l">
+				<img src="images/alvas.png" alt="" />
+			</div>
+		</div>
+	</div>
+	<!-- //register -->
+
+	<!-- //register -->
+	<?php
+include("footer.php")
+?>
---
+++ adminlogin.php
@@ -0,0 +1,66 @@
+<?php
+include("header.php");
+if(isset($_SESSION["adminid"]))
+{
+	echo "<script>window.location='dashboard.php';</script>";
+}
+if(isset($_POST["submit"]))
+{
+	$sql="SELECT * FROM admin where loginid='$_POST[loginid]' AND password='$_POST[password]' AND status='Active'";
+	$qsql=mysqli_query($con,$sql);
+	echo mysqli_error($con);
+	
+	if(mysqli_num_rows($qsql)==1)
+	{
+		$rslogin = mysqli_fetch_array($qsql);
+		$_SESSION["adminid"] = $rslogin[adminid];
+		$_SESSION["usertype"] = $rslogin[usertype];
+		echo "<script>window.location='dashboard.php';</script>";
+	}
+	else
+	{
+		echo"<script>alert('Invalid credentials entered..');</script>";
+	}
+}
+?>
+	<!-- register -->
+	<div class="register-sec-w3l" id="register">
+		<div class="container">
+			<div class="col-md-6 book-appointment">
+				<h3>Admin Login form</h3>
+				<form action="" method="post"name="frm" onsubmit="return validateform()">
+					<div class="gaps">
+						<p>Login ID</p>
+						<input type="text" name="loginid"  autocomplete="off" placeholder="Enter Login ID" />
+					</div>
+					<div class="gaps">
+						<p>Password</p>
+						<input type="password" name="password" placeholder="Enter Password" id="password1"/>
+					<input type="submit" name="submit" value="Login">
+				</form>
+			</div>
+			
+		</div>
+	</div>
+	<!-- //register -->
+
+
+	<?php
+include("footer.php")
+?>
+<script>
+function validateform()
+	{
+		if(document.frm.loginid.value == "")
+		{
+			alert("Login ID should not be empty..");
+			return false;
+		}
+		else if(document.frm.password.value == "")
+		{
+			alert("Please enter the password..");
+			return false;
+		}
+	
+	}
+	</script>
